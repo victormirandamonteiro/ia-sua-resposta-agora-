43c1a3d8dc0eba455bf93fbb0643a86488bfabf4
victormirandamonteiro <143108704+victormirandamonteiro@users.noreply.github.com>
2025-10-02 19:16:24 -0300

    fim

--- script.js
+++ script.js
@@ -85,13 +85,33 @@ function mostraPergunta(){
 
     }
     perguntaAtual=perguntas[atual];
-    caixaPerguntas.tex
-
-
-
-
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternstivas.textContent="";
+    mostraAlternativa();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
 
 
 
